7efd504e6e61b484e35666ae317ece6cd563815f
joedupor <joedupor@gmail.com>
2023-09-07 17:12:47 -0500

    init commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Give a temperature in F degrees to be converted to C degrees:");
+            double fTemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(TempConverter.FahrenheitToCelsius(fTemp), 1));
+            Console.WriteLine();
+
+
+
+
+            Console.WriteLine("Give a temperature in C degrees to be converted to F degrees:");
+            double cTemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(TempConverter.CelsiusToFahrenheit(cTemp), 1));
+
+
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double temp)
+        {
+            double convertedTemp = ((temp - 32) * 5) / 9;
+            return convertedTemp;
+        }
+
+
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            double convertedTemp = (((temp * 9) / 5)) + 32;
+            return convertedTemp;
+        }
+
+
+    }
+}
